41518d794d3ed9e6707bf437034d93645c7eee9f
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-03 14:31:49 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 cd src
 nasm -f elf32 kernel.asm -o kasm.o
 cd -
-gcc -Wall -Wextra -fno-stack-protector -m32 -c src/kernel.c -o kc.o -ffreestanding -nostdlib -nostdinc -fno-builtin 
+gcc -Wall -Wextra -fno-stack-protector -m32 -c src/kernel.c -o kc.o -ffreestanding -nostdlib
 
 ld -m elf_i386 -T link.ld -o kernel.elf src/kasm.o kc.o 
 
