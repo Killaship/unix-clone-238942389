bdc1f361e99fc6f73ce0396b751c70c934eb4ea9
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-02 21:32:12 -0500

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+cd src
+nasm -f elf32 kernel.asm -o kasm.o
+cd -
+gcc -Wall -Wextra -fno-stack-protector -m32 -c src/kernel.c -o kc.o -ffreestanding
+
+ld -m elf_i386 -T link.ld -o kernel.elf src/kasm.o kc.o -format binary initrd.o
+
+mkdir -p iso/boot/grub
+cp kernel.elf iso/boot/kernel.elf
+cp grub.cfg iso/boot/grub/grub.cfg
+grub-mkrescue -o os.iso iso
